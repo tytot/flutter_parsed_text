59d059c1ed34d9e4b2bd597ed2ee29c639658e4c
Tyler Lin <tylerhlin@gmail.com>
2024-05-11 17:46:31 -0400

    Make ParsedTextSpan a class

--- lib/flutter_parsed_text.dart
+++ lib/flutter_parsed_text.dart
@@ -3,6 +3,7 @@ library flutter_parsed_text;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
+part 'src/parsed_text_span.dart';
 part 'src/parsed_text.dart';
 part 'src/regex_options.dart';
 part 'src/match_text.dart';
